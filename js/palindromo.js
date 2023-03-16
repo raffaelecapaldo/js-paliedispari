0f8273798bcd9fa1533e72459645da61ab3895bd
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-16 14:53:26 +0100

    Added html result to checkPalindrome()

--- js/palindromo.js
+++ js/palindromo.js
@@ -4,8 +4,10 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 const world = document.getElementById("world");
 const checkButton = document.getElementById("check-world");
+const result = document.getElementById("result");
 
 world.addEventListener("keyup", checkValidity)
+checkButton.addEventListener("click", checkPalindrome);
 
 function checkValidity() { //Controlla che l'input non sia una stringa vuota o un numero
     if (!isNaN(world.value) || (!world.value)) {
@@ -23,17 +25,17 @@ function checkValidity() { //Controlla che l'input non sia una stringa vuota o u
 function reverseString(str) {//Data una stringa la restituisce al contrario
     return str.split('') // Separa ogni lettera della parola
     .reverse()  //Inverti l'ordine delle lettere
-    .join('');;// Riuniscile assieme
+    .join('');// Riuniscile assieme
 }
 
 
 function checkPalindrome() {//Controlla se è palindroma
     const reverseWorld = reverseString(world.value);//Prendi il valore dell'input e manipolalo con la funzione
     if (world.value === reverseWorld) {//Se il risultato coincide con la parola inserita = palindroma
-        console.log("è palindroma")
+        result.innerHTML = `<span class="text-success fs-5 fw-bold">La parola è palindroma!</span>`;
     }
     else {
-        console.log("non è palindroma")//Altrimenti non palindroma
+        result.innerHTML = `<span class="text-danger fs-5 fw-bold">La parola non è palindroma!</span>`;
     }
 }
 
