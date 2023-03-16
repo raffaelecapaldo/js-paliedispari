a4ee5ccaf4ecb6c5bc45719c723ef06f80e0c7c4
raffaelecapaldo <raffaelecpl@gmail.com>
2023-03-16 17:10:20 +0100

    Added JS validation

--- js/paridispari.js
+++ js/paridispari.js
@@ -10,31 +10,46 @@ const evenOrOdd = document.querySelector("select");
 const buttonPlay = document.getElementById("play-button");
 const infoBox = document.getElementById("info-box");
 
-buttonPlay.addEventListener ("click", playGame);
+buttonPlay.addEventListener("click", playGame);
+userNumber.addEventListener("keyup", checkCanPlay);
+evenOrOdd.addEventListener("change", checkCanPlay);
 
 function playGame() {
-    const pcNumber = getRndNumber(1,5);//Genera numero random
-    const sumNumbers = addNumbers(parseInt(userNumber.value),pcNumber);//Somma numero utente con numero generato
+    const pcNumber = getRndNumber(1, 5);//Genera numero random
+    const sumNumbers = addNumbers(parseInt(userNumber.value), pcNumber);//Somma numero utente con numero generato
     const checkedOddEven = oddOrEven(sumNumbers);//Controlla se è pari o dispari
     //Scrivi le info in HTML
-        infoBox.innerHTML = `<div id="result-box" class="w-100">
+    infoBox.innerHTML = `<div id="result-box" class="w-100">
         <p class="toshow del1 pt-2 m-0">Il numero del computer è: <span class="fw-bold">${pcNumber}</span></p>
         <p class="toshow del2 m-0 pt-1">La somma è: <span class="fw-bold">${sumNumbers}</span></p>
         <p class="toshow del3 m-0 pt-1">Hai giocato: <span class="fw-bold text-success text-uppercase">${evenOrOdd.selectedOptions[0].text}</span></p>`
-        if (checkedOddEven === evenOrOdd.value) {//Se utente ha scelto PARI e la somma è PARI vince, altrimenti perdi
-            infoBox.innerHTML += `<p class="toshow text-success pt-3 fw-bold text-center fs-4">HAI VINTO!</p>`
-        }
-        else {
-            console.log("Hai perso");
-            infoBox.innerHTML += `<p class="toshow text-danger pt-3 fw-bold text-center fs-4">HAI PERSO!</p>`
-        }
+    if (checkedOddEven === evenOrOdd.value) {//Se utente ha scelto PARI e la somma è PARI vince, altrimenti perdi
+        infoBox.innerHTML += `<p class="toshow text-success pt-3 fw-bold text-center fs-4">HAI VINTO!</p>`
+    }
+    else {
+        console.log("Hai perso");
+        infoBox.innerHTML += `<p class="toshow text-danger pt-3 fw-bold text-center fs-4">HAI PERSO!</p>`
+    }
+}
+
+if (evenOrOdd.value == "odd") {//Come sopra ma con DISPARI
+    if (checkedOddEven === evenOrOdd.value) {
+        infoBox.innerHTML += `<p class="toshow text-success pt-3 fw-bold text-center fs-4">HAI VINTO!</p>`
+    }
+    else {
+        infoBox.innerHTML += `<p class="toshow text-danger pt-3 fw-bold text-center fs-4">HAI PERSO!</p>`
+    }
+}
+
+
+function checkCanPlay() {
+    if (isNaN(userNumber.value) || parseInt(userNumber.value) < 1 || parseInt(userNumber.value) > 5 || evenOrOdd.value === "") {
+
+
+        buttonPlay.classList.add("disabled");
     }
 
-    if (evenOrOdd.value == "odd") {//Come sopra ma con DISPARI
-        if (checkedOddEven === evenOrOdd.value) {
-            infoBox.innerHTML += `<p class="toshow text-success pt-3 fw-bold text-center fs-4">HAI VINTO!</p>`
-        }
-        else {
-            infoBox.innerHTML += `<p class="toshow text-danger pt-3 fw-bold text-center fs-4">HAI PERSO!</p>`
-        }
+    else {
+        buttonPlay.classList.remove("disabled");
     }
+}
